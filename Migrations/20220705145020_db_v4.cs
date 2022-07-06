9d220d21c435e5c062fcd0fe25fc1abb8149ae3e
Divu2000 <kaurdivneet2000@gmail.com>
2022-07-06 06:31:59 +0000

    init

---
+++ Migrations/20220705145020_db_v4.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DailyMutualFundNAV.Migrations
+{
+    public partial class db_v4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
